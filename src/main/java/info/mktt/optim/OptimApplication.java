da5463b7a3b6c2ff671346a043a3698f186ede5d
takku66 <tattaka15@gmail.com>
2023-05-20 02:51:11 +0900

    初期構築：正常にrunできるところまで確認

---
+++ src/main/java/info/mktt/optim/OptimApplication.java
@@ -0,0 +1,13 @@
+package info.mktt.optim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OptimApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OptimApplication.class, args);
+	}
+
+}
---
+++ src/test/java/info/mktt/optim/OptimApplicationTests.java
@@ -0,0 +1,13 @@
+package info.mktt.optim;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OptimApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
