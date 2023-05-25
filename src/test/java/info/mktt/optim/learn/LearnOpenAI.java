2e5fcccf4a04d2fb3e734eb889c518cbe6bd1043
takku66 <tattaka15@gmail.com>
2023-05-25 17:15:04 +0900

    コメントを加筆

--- src/test/java/info/mktt/optim/learn/LearnOpenAI.java
+++ src/test/java/info/mktt/optim/learn/LearnOpenAI.java
@@ -52,10 +52,10 @@ public class LearnOpenAI {
 
         // レスポンスを取得し、正常・異常問わず結果をコンソールに出力する
         try(BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));){
-            String successResponse = extractInputStream(in);
+            String successResponse = extractOneLine(in);
             System.out.println(successResponse.toString());
         }catch(IOException e){
-            String errorResponse = extractInputStream(con.getInputStream());
+            String errorResponse = extractOneLine(con.getInputStream());
             System.out.println(errorResponse);
             throw e;
         }finally{
@@ -64,14 +64,26 @@ public class LearnOpenAI {
 
     }
 
-    private String extractInputStream(InputStream in) throws IOException{
+    /**
+     * <p>InputStreamを読み取り、1行の文字列を返す。
+     * @param in 抽出対象のストリーム
+     * @return 抽出した1行の文字列
+     * @throws IOException
+     */
+    private String extractOneLine(InputStream in) throws IOException{
 
         try(BufferedReader br = new BufferedReader(new InputStreamReader(in));){
-            return extractInputStream(br);
+            return extractOneLine(br);
         }
     }
 
-    private String extractInputStream(BufferedReader br) throws IOException{
+    /**
+     * <p>BufferedReaderを読み取り、1行の文字列を返す。
+     * @param br 抽出対象のBufferedReader
+     * @return 抽出した1行の文字列
+     * @throws IOException
+     */
+    private String extractOneLine(BufferedReader br) throws IOException{
 
         String inputLine;
         StringBuffer sb = new StringBuffer();
@@ -85,11 +97,24 @@ public class LearnOpenAI {
         return sb.toString();
     }
 
+    /**
+     * <o>指定されたURLに接続し、接続状態のHttpURLConnectionオブジェクトを返す。
+     * <p>接続に失敗した場合は、例外を発生させる。
+     * @param url 接続したいURL
+     * @return 接続状態のHttpURLConnectionオブジェクト
+     * @throws IOException 接続に失敗した場合に発生する例外
+     * @see HttpURLConnection
+     */
     private HttpURLConnection createConnection(String url) throws IOException{
         var obj = new URL(url);
         return (HttpURLConnection) obj.openConnection();
     }
 
+    /**
+     * リクエストプロパティを設定する
+     * @param con リクエスト送信前のコネクション
+     * @throws ProtocolException リクエストメソッドが正しく設定されなかった場合に発生する例外
+     */
     private void writeProperty(HttpURLConnection con) throws ProtocolException{
         var API_KEY = environment.getProperty(PROPERTY_API_KEY);
         var ORGANIZATION_KEY = environment.getProperty(PROPERTY_ORGANIZATION_KEY);
@@ -100,6 +125,12 @@ public class LearnOpenAI {
         con.setDoOutput(true);
     }
 
+    /**
+     * 正常系のリクエストボディを設定する
+     * @param con リクエスト送信前のコネクション
+     * @throws UnsupportedEncodingException
+     * @throws IOException
+     */
     private void writeBodyForValidParameter(HttpURLConnection con) throws UnsupportedEncodingException, IOException{
         var sendData = new HashMap<String, Object>();
         var messages = new ArrayList<Object>();
@@ -118,6 +149,12 @@ public class LearnOpenAI {
         writeConnection(con, json);
     }
 
+    /**
+     * リクエストボディに書き込む
+     * @param con リクエスト送信前のコネクション
+     * @param json リクエストボディに書き込むJson文字列
+     * @throws IOException 書き込めなかった場合の例外
+     */
     private void writeConnection(HttpURLConnection con, String json) throws IOException{
         // リクエストのbodyにJSON文字列を書き込む
         var out = new OutputStreamWriter(con.getOutputStream(), "UTF-8");
